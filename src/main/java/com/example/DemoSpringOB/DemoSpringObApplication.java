9abc36ea98983716eb17a8a3f454f8097ec87849
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-04-03 16:01:40 +0100

    iniciamos proyecto

---
+++ src/main/java/com/example/DemoSpringOB/DemoSpringObApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoSpringOB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringObApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringObApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoSpringOB/DemoSpringObApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoSpringOB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringObApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
