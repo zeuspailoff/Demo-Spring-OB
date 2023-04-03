5491839a963df402861dc8340936c502ac38b962
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-04-03 16:46:01 +0100

    iniciamos Swagger

---
+++ src/main/java/com/example/DemoSpringOB/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+package com.example.DemoSpringOB.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Configuración Swagger para la generación de documentación de la API REST
+ *
+ *http://localhost:8080/swagger-ui/
+ *http://localhost:8080/v2/api-docs
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiDetails())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiDetails(){
+        return new ApiInfo("Spring Boot Book API REST",
+                "Library Api rest docs",
+                "1.0",
+                "http://www.google.com",
+                new Contact("Alan", "http://www.google.com", "alan@example.com"),
+                "MIT",
+                "http://www.google.com",
+                Collections.emptyList());
+    }
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+}
