58af1399e7d5e1510e04adf2d844f84b15744e22
Abraham Díaz <114292541+zeuspailoff@users.noreply.github.com>
2023-04-06 11:34:29 +0100

    preparamos seguridad

--- src/main/java/com/example/DemoSpringOB/config/WebSecurityConfig.java
+++ src/main/java/com/example/DemoSpringOB/config/WebSecurityConfig.java
@@ -1,5 +1,45 @@
 package com.example.DemoSpringOB.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().requestMatchers("/boot").hasRole("USER")
+                .and().formLogin()
+                .usernameParameter("admin")
+                .passwordParameter("password")
+                .loginPage("/authentication/login")
+                .failureUrl("/authentication/login?failed")
+                .loginProcessingUrl("/authentication/login/process");
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
 }
+
